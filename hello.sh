1c436f8da701d78587ab7a96e9327cd3982b9cb4
Leon Ljunghorn <leonljunghorn@gmail.com>
2022-10-19 22:24:47 +0200

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,4 +1,18 @@
-#### Hello
-clear; echo; echo; echo; read -ep "  - ARE YOU OK?:" -i " ...no "  qqok; echo; echo; if [[ ${qqok} = " ...no" ]]; then echo "...oh no";else echo "k"; fi;
 
 
+##########################
+## OK? p-{qqok} ##########
+##########################
+clear; echo; echo; echo; echo; read -ep "      - ARE YOU OK?:" -i " ...no "  qqok; echo; echo; if [[ ${qqok} = "...no" ]]; then echo "...oh no";else echo "k"; fi;
+###########################
+###########################
+
+
+
+
+##########################
+## LAPTOPLID-FIX #########
+##########################
+read -ep "laptop-lid-fix?: " -i "Yes" qqltl; if [[ ${qqltl} = "Yes" ]]; then sudo -s ; echo 'HandleLidSwitch=ignore' >> /etc/systemd/logind.conf; else echo "k"; fi; 
+###########################
+###########################
